38d66081b741d5a9459baa12272d3b49f7606af5
Bartosz Małyjasiak <bartosz.malyjasiak@gmail.com>
2023-09-12 00:34:29 +0200

    add dto validation

--- src/main/java/com/koparyProjekt/koparyProjekt/controller/KoparaController.java
+++ src/main/java/com/koparyProjekt/koparyProjekt/controller/KoparaController.java
@@ -1,9 +1,12 @@
 package com.koparyProjekt.koparyProjekt.controller;
 
 import com.koparyProjekt.koparyProjekt.data.Kopara;
+import com.koparyProjekt.koparyProjekt.dto.KoparaDto;
 import com.koparyProjekt.koparyProjekt.service.KoparaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,20 +18,28 @@ public class KoparaController {
     private KoparaService koparaService;
 
     @GetMapping
-    public List<Kopara> findAll() {
-        return koparaService.findAll();
+    public ResponseEntity<List<Kopara>> findAll() {
+        return ResponseEntity.ok(koparaService.findAll());
     }
     @GetMapping("/{id}")
-    public Kopara findById(@PathVariable @Valid long id) {
-        return koparaService.findById(id);
+    public ResponseEntity<Kopara> findById(@PathVariable @Valid long id) {
+
+        return ResponseEntity.ok(koparaService.findById(id));
     }
     @PostMapping
-    public Kopara create(@RequestBody @Valid Kopara kopara) {
-        return koparaService.save(kopara);
+    public ResponseEntity<Kopara> create(@RequestBody @Valid KoparaDto koparaRequest) {
+
+        return new ResponseEntity<>(koparaService.save(koparaRequest), HttpStatus.CREATED);
     }
     @PutMapping("/{id}")
-    public Kopara update(@RequestBody @Valid Kopara kopara) {
-        return koparaService.save(kopara);
+    public ResponseEntity<Kopara> update(@PathVariable Long id, @RequestBody @Valid Kopara kopara) {
+        Kopara updatedKopara = koparaService.update(id, kopara);
+
+        if (updatedKopara != null) {
+            return new ResponseEntity<>(updatedKopara, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable @Valid long id){
